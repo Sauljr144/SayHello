f81299966d4c198763aaab066abdbb80bc82c9d2
Sauljr144 <saulsuazojr06@gmail.com>
2022-10-19 20:05:32 -0700

    code change

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
-﻿//Say Hello
+﻿//---- Challange #1 ----//
 
-Console.WriteLine("Hello!");
+//Challege is to be able to input a name and have the computer say "Hello ______"
+
+//----Say Hello----//
+
+//This is a variable to store the input of the user//
+string name1;
+
+//This will display the question "What is your name?"//
+Console.WriteLine("What is your name?");
+
+//stores users input
+name1 = Console.ReadLine();
+
+//this wiill display users input for example "Hello, T-1000" by using interpolatioon
+
+Console.WriteLine($"Hello, {name1}");
+
+
+//Peer Reviewed by Amir Smith 08/09/22
